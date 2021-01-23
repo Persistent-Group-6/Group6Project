fac6def9f5294aef19529df27c7eeccfcd1ac722
sushmita <sushmita_raj@persistent.co.in>
2021-01-23 21:57:50 +0530

    Connection Class Added

--- src/com/psl/training/util/DBConnection.java
+++ src/com/psl/training/util/DBConnection.java
@@ -1,5 +1,44 @@
 package com.psl.training.util;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class DBConnection {
+	static Connection cn=null;
+
+	private DBConnection(){
+		
+	}
+	static String driverClass="com.mysql.jdbc.Driver";
+	static String url="jdbc:mysql://localhost/order_management_system_db";
+	static String username="root";
+	static String password="123";
+	
+	
+	public static Connection getConnection(){
+		if(cn==null){
+			// Optional
+			try {
+				Class.forName(driverClass);
+				cn=DriverManager.getConnection(url,username,password);
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
+					}
+		
+		return cn;
+		
+	
+	}
+	
+	public void closeConnection(){
+		try {
+			cn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
