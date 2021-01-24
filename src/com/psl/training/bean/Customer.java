ffe1c5cb69185fa618f746a3c8133c5a01117977
sushmita <sushmita_raj@persistent.co.in>
2021-01-24 12:58:24 +0530

    Customer bean class printing address & numbers methods added

--- src/com/psl/training/bean/Customer.java
+++ src/com/psl/training/bean/Customer.java
@@ -3,19 +3,38 @@ package com.psl.training.bean;
 import java.util.List;
 
 public class Customer {
+	//variables
 	private int customerNumber;
 	private String customerName;
-	
 	private String homePhone;
 	private String cellPhone;
 	private String workPhone;
 	private String street;
 	private String city;
+	private String state;
 	private String zip;
 	private List<PurchaseOrder> po;
+	
+	private String printingAddress;
+	private String phoneNumbers;
 
 	public Customer() {
 	// TODO Auto-generated constructor stub
+	}
+
+	public Customer(int customerNumber, String customerName, String homePhone, String cellPhone, String workPhone,
+			String street, String city,String state, String zip, List<PurchaseOrder> po) {
+		super();
+		this.customerNumber = customerNumber;
+		this.customerName = customerName;
+		this.homePhone = homePhone;
+		this.cellPhone = cellPhone;
+		this.workPhone = workPhone;
+		this.street = street;
+		this.city = city;
+		this.state=state;
+		this.zip = zip;
+		this.po = po;
 	}
 
 	public int getCustomerNumber() {
@@ -73,6 +92,13 @@ public class Customer {
 	public void setCity(String city) {
 		this.city = city;
 	}
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
 
 	public String getZip() {
 		return zip;
@@ -95,7 +121,40 @@ public class Customer {
 		return "Customer [customerNumber=" + customerNumber + ", customerName=" + customerName + ", homePhone="
 				+ homePhone + ", cellPhone=" + cellPhone + ", workPhone=" + workPhone + ", street=" + street + ", city="
 				+ city + ", zip=" + zip + ", po=" + po + "]";
+	} 
+	
+	//setPhoneNumbers
+	public void setPhoneNumbers(int customerNumber) {
+		if(this.customerNumber==customerNumber) {
+			 this.phoneNumbers="HomePhone"+homePhone+"CellPhone"+cellPhone+"WorkPhone"+workPhone;
+
+		}
 	}
+	 public String printPhoneNumbersString(int customerNumber) {
+		 if(this.customerNumber==customerNumber) {
+			 	return this.phoneNumbers;
+		}
+		return null;
+	 }
+	 
+	 //Address
+	 public void setPrintingAddress(int customerNumber) {
+			if(this.customerNumber==customerNumber) {
+				 this.printingAddress="Street-"+street+"City-"+city+"State-"+state+"Zip"+zip;
+
+			}
+	 }
+	 public String printShippingAddress(int customerNumber) {
+		 if(this.customerNumber==customerNumber) {
+			 return this.printingAddress;
+		 }
+		 return null;
+	 }
+	 
+	 public void print(Customer c) {
+		 System.out.println(c);
+	 }
+	 
 
 
 
